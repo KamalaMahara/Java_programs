c0b2c8ec20b046d8015ce147f2958e561a9b4181
kamala mahara <kmlamahara@gmail.com>
2025-06-17 13:43:17 +0545

    for each loop program added

---
+++ controlStatements/looping/forEachLoop.java
@@ -0,0 +1,10 @@
+
+class forEachLoop {
+
+    public static void main(String[] args) {
+        String[] names = {"mango", "apple", "litchi", "papaya"};
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
