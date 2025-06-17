d29e944f0f5cd5f36f940e9aefe9d926fe30f39f
kamala mahara <kmlamahara@gmail.com>
2025-06-17 13:56:07 +0545

    break statement program added

---
+++ controlStatements/branching/breakStatement.java
@@ -0,0 +1,14 @@
+
+class breakStatement {
+
+    public static void main(String[] args) {
+        for (int j = 0; j < 5; j++) {
+            if (j == 4) {
+                break;
+            }
+            System.out.println(j);
+
+        }
+        System.out.println("After the loop");
+    }
+}
