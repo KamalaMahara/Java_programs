4b59fb48fe07ff7e5d9607666551e5bcce8f0118
kamala mahara <kmlamahara@gmail.com>
2025-06-17 13:32:20 +0545

    do while loop program added

---
+++ controlStatements/looping/dowhileLoop.java
@@ -0,0 +1,13 @@
+
+class dowhileLoop {
+
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println("value of i is:" + i);
+            i++;
+
+        } while (i <= 10);
+        System.out.println("looop ended");
+    }
+}
