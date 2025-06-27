9c9e46960fb3ef6e064169186794f17c002c965f
kamala mahara <kmlamahara@gmail.com>
2025-06-27 12:44:27 +0545

    modified program

--- oop/useNum.java
+++ oop/useNum.java
@@ -1,4 +1,6 @@
 
+import java.util.*;
+
 class Num {
 
     int i, j;
@@ -22,8 +24,14 @@ class Num {
 public class useNum {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the two numbers:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("before swap: a=" + a + "b=" + b);
+
         Num obj = new Num();
-        obj.setnum(10, 20);
+        obj.setnum(a, b);
         obj.swap(obj);
         obj.show();
 
