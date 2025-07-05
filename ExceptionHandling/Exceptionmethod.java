6b836328b670fde8908f1dab4f58a0256eda21fa
kamala mahara <kmlamahara@gmail.com>
2025-07-05 23:11:45 +0545

    printStackTrace() used in exception handling

---
+++ ExceptionHandling/Exceptionmethod.java
@@ -0,0 +1,12 @@
+
+public class Exceptionmethod {
+
+    public static void main(String[] args) {
+        try {
+            int result = 10 / 0; // This will cause ArithmeticException
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+        }
+    }
+}
